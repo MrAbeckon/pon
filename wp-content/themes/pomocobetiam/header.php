154ca451d5cb0ca92eb416f7772868848c8e05af
Martin Albert <albert@killkitsch.com>
2020-02-23 17:10:14 +0100

    header.php - header modified to bootstrap

--- wp-content/themes/pomocobetiam/header.php
+++ wp-content/themes/pomocobetiam/header.php
@@ -17,7 +17,7 @@
           <?php 
           if(is_front_page() || is_home()){
             echo "Pomoc Obetiam Násilia";
-          } else{
+          } else {
             echo wp_title('');
           }
           echo ' | Pomoc obetiam násilia'; ?>
@@ -30,7 +30,7 @@
 
     </head>
 
-    <body class="">
+    <body>
         <?php
         $current_uri = home_url(add_query_arg(NULL, NULL));
 
@@ -53,18 +53,90 @@
 
         <!-- NAVIGATION BAR -->
         <header class="page-head">
-          <nav>
-            <ul>
-              <li>menu</li>
-            </ul>
+          <nav class="page-head-nav navbar navbar-expand-lg">
+            <div class="page-head-nav-logo">
+              <a class="navbar-brand" href="#">
+                <img src="<?php echo get_template_directory_uri() ?>/images/logo.jpg" alt="POMOC OBETIAM LOGO" />
+              </a>
+              <div class="page-head-nav-headings">
+                <h5>Pomoc Obetiam Násilia</h5>
+                <p>Občianske združenie</p>
+                <p>Victim Support Slovakia</p>
+              </div>
+              
+            </div>
+            <div class="collapse navbar-collapse justify-content-end">
+              <ul class="page-head-nav-list navbar-nav">
+                <?php
+                $locations = get_nav_menu_locations();
+                if ( isset( $locations['header']) ) {
+                  $menu = get_term( $locations['header'], 'nav_menu');
+                }
+                if ( $items = wp_get_nav_menu_items( $menu->name ) ) {
+                  foreach ( $items as $item ) {
+                    if ($item->menu_item_parent != 0) {
+                      continue;
+                    }
+                    $curNavItemID = $item->ID;
+                    $classList = implode(" ", $item->classes); 
+                    $item_url = $item->url; ?>
+                    
+                    <li class="page-head-nav-list-item nav-item">
+                      <a href="<?php echo $item_url ?>"><?php echo $item->title ?></a>
+
+                    <?php if ( in_array('dropdown', $item->classes)) { ?>
+                      
+                      <ul class="page-head-nav-list-item-dropdown dropdown dropdown-menu <?php echo $item->classes[1]; ?>">
+                        
+                        <?php foreach($items as $subnav) {
+                          $curSubItemID = $subnav->ID;
+                          $item_url = $subnav->url;
+                          if ( $subnav->menu_item_parent == $curNavItemID) { ?>
+                              
+                              <li class="dropdown-item">
+                              <div class=""><a href="<?php echo $subnav->url ?>"><?php echo $subnav->title; ?></a></div>
+                          
+                          <?php if ( in_array('dropdown-list', $subnav->classes)) {?>
+                                <ul class="dropdown-list style-none">
+                              <?php foreach($items as $subsubnav) { 
+                                    if ( $subsubnav->menu_item_parent == $curSubItemID ) { ?>
+                                      <li>
+                                        <a href="<?php echo $subsubnav->url ?>">
+                                          <div class="menu-nadpisy-h5"><?php echo $subsubnav->title ?></div>
+                                          <?php echo mb_strimwidth($subsubnav->description, 0, 100, "...") ?>
+                                        </a>
+                                      </li>
+                              <?php } elseif ($subsubnav == end($items)) { ?>
+                                    <li><a href="<?php echo $item_url; ?>">Více ...</a>
+                              <?php }
+                                  }
+                                  echo "</ul>";
+                                }
+                              echo "</li>";
+                              }
+                            }
+                          echo "</ul>";
+                          } 
+                        ?>
+                      </li>
+                    <?php  
+                    } 
+                  } else {
+                    echo "MENU NOT FOUND";   
+                    var_dump($locations);
+                    var_dump($menu);
+                        }
+                      ?>
+              </ul>
+            </div>
           </nav>
         </header>
 
         <!-- START OF PAGE -->
-        <main class="page-main">
+        <main class="page-main container">
             <?php 
 
-          // HEADER_IMAGE OR
+          // NOT HOME
           if(!is_home()) { ?>
             <section class="header-image section-40 section-sm-40 section-md-66 bg-gray-dark page-title-wrap">
               <div class="shell">
@@ -89,15 +161,15 @@
              
         // START OF LAYOUT ?>
 
-            <section>
+            <section class="row">
 
             <?php
                 if (!is_home()): ?>
-                <div class="">
+                <div class="col-12">
 
                 <?php
                 else: ?>
-                <div class="">
+                <div class="col-12">
                 
                 <?php
                 endif; ?>
