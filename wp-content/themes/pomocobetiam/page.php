89933cafc15291047c41d7b9f08a5f3c43bb87b1
Martin Albert <albert@killkitsch.com>
2020-02-29 00:45:20 +0100

    base page.php created

--- wp-content/themes/pomocobetiam/page.php
+++ wp-content/themes/pomocobetiam/page.php
@@ -5,7 +5,13 @@
 <!-- END OF CATEGORY HEADER -->
 <!-- START OF PAGE -->
 <section class="">
-    
+	<?php 
+	if(have_posts()):
+		while(have_posts()):
+			the_post(); ?>
+			<?php the_content(); 
+		endwhile; 
+	endif; ?>
 </section>
 
 <?php get_footer(); ?>
