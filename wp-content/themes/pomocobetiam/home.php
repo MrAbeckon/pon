2bd7ea3d2c328903e9c758af1eb281e89d518f39
Martin Albert <albert@killkitsch.com>
2020-02-29 00:45:11 +0100

    base skeleton for home created

--- wp-content/themes/pomocobetiam/home.php
+++ wp-content/themes/pomocobetiam/home.php
@@ -1,8 +1,162 @@
 <?php get_header(); ?>
 
-	<h1 class="text-center">HELLO</h1>
+	<!-- INTRO SECTION -->
+	<div class="home-linka">
+		<p>
+			nonstop
+			</br>
+			linka pomoci obetiam
+		</p>
+		<h1 class="text-center">0944 254 405</h1>
+	</div>
+	<div class="home-intro">
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem molestias quod vel porro voluptatibus? Porro dolor sapiente maxime provident adipisci labore obcaecati temporibus corporis laboriosam quis? Id doloribus aliquid assumenda?
+		</p>
+		<button class="button button-pink" type="button">
+			požiadajte o podporu online
+		</button>
+	</div>
 
 	</div>
 </section>
+
+<section class="row block block-pink">
+	<div class="col-8 offset-2">
+		<h2>Lorem</h2>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque fugiat aspernatur odit harum nulla velit minima obcaecati eveniet, nesciunt omnis molestias, sunt ratione fuga temporibus architecto magnam aut dolores. Facilis!
+		</p>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque fugiat aspernatur odit harum nulla velit minima obcaecati eveniet, nesciunt omnis molestias, sunt ratione fuga temporibus architecto magnam aut dolores. Facilis!
+		</p>
+	</div>
+</section>
+
+<section class="row block block-blue">
+	<div class="col-8 offset-2 block-inner">
+		<h2>Lorem</h2>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque fugiat aspernatur odit harum nulla velit minima obcaecati eveniet, nesciunt omnis molestias, sunt ratione fuga temporibus architecto magnam aut dolores. Facilis!
+		</p>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque fugiat aspernatur odit harum nulla velit minima obcaecati eveniet, nesciunt omnis molestias, sunt ratione fuga temporibus architecto magnam aut dolores. Facilis!
+		</p>
+	</div>
+	<div class="col-8 offset-2 block-inner">
+		<h3>Lorem</h3>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque fugiat aspernatur odit harum nulla velit minima obcaecati eveniet, nesciunt omnis molestias, sunt ratione fuga temporibus architecto magnam aut dolores. Facilis!
+		</p>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque fugiat aspernatur odit harum nulla velit minima obcaecati eveniet, nesciunt omnis molestias, sunt ratione fuga temporibus architecto magnam aut dolores. Facilis!
+		</p>
+	</div>
+	<div class="col-8 offset-2 block-inner">
+		<button class="button button-blue" type="button">
+			Kontaktujte nás
+		</button>
+		<div class="contact-info">
+			<div class="contact-info-item">
+				<i class="fa" aria-hidden="true"></i>
+				<a href="#">0944 254 405</a>
+			</div>
+			<div class="contact-info-item">
+				<i class="fa" aria-hidden="true"></i>
+				<a href="#">info@pomocobetiam.sk</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="row block block-transparent">
+	<div class="col-8 offset-2 block-inner">
+		<h2>Lorem</h2>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque fugiat aspernatur odit harum nulla velit minima obcaecati eveniet, nesciunt omnis molestias, sunt ratione fuga temporibus architecto magnam aut dolores. Facilis!
+		</p>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque fugiat aspernatur odit harum nulla velit minima obcaecati eveniet, nesciunt omnis molestias, sunt ratione fuga temporibus architecto magnam aut dolores. Facilis!
+		</p>
+	</div>
+	<div class="col-8 offset-2 block-inner">
+		<h3>Lorem</h3>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque fugiat aspernatur odit harum nulla velit minima obcaecati eveniet, nesciunt omnis molestias, sunt ratione fuga temporibus architecto magnam aut dolores. Facilis!
+		</p>
+	</div>
+	<div class="col-12 block-inner">
+		<div class="block-inner-list">
+
+		</div>
+	</div>
+	<div class="col-8 offset-2">
+		<button class="button button-blue" type="button">
+			Kontaktujte nás
+		</button>
+	</div>
+</section>
+
+<section class="row block block-blue" id="faq">
+	<div class="col-8 offset-2 block-inner">
+		<h2>Lorem</h2>
+		
+		<div class="question">
+			<h4>Lorem?</h4>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque fugiat aspernatur odit harum nulla velit minima obcaecati eveniet, nesciunt omnis molestias, sunt ratione fuga temporibus architecto magnam aut dolores. Facilis!
+			</p>
+		</div>
+
+		<div class="question">
+			<h4>Lorem?</h4>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque fugiat aspernatur odit harum nulla velit minima obcaecati eveniet, nesciunt omnis molestias, sunt ratione fuga temporibus architecto magnam aut dolores. Facilis!
+			</p>
+		</div>
+
+		<div class="question">
+			<h4>Lorem?</h4>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque fugiat aspernatur odit harum nulla velit minima obcaecati eveniet, nesciunt omnis molestias, sunt ratione fuga temporibus architecto magnam aut dolores. Facilis!
+			</p>
+		</div>
+
+		<div class="question">
+			<h4>Lorem?</h4>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque fugiat aspernatur odit harum nulla velit minima obcaecati eveniet, nesciunt omnis molestias, sunt ratione fuga temporibus architecto magnam aut dolores. Facilis!
+			</p>
+		</div>
+	</div>
+</section>
+
+<section class="row block block-pink">
+	<div class="col-8 offset-2 block-inner">
+		<h2>Lorem</h2>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque fugiat aspernatur odit harum nulla velit minima obcaecati eveniet, nesciunt omnis molestias, sunt ratione fuga temporibus architecto magnam aut dolores. Facilis!
+		</p>
+	</div>
+	<div class="col-4 offset-2 block-inner">
+		<h4>Lorem</h4>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque fugiat aspernatur odit harum nulla velit minima obcaecati eveniet, nesciunt omnis molestias, sunt ratione fuga temporibus architecto magnam aut dolores. Facilis!
+		</p>
+		<button class="button button-pink" type="button">
+			požiadajte o podporu online
+		</button>
+	</div>
+	<div class="col-4 block-inner">
+		<h4>Lorem</h4>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque fugiat aspernatur odit harum nulla velit minima obcaecati eveniet, nesciunt omnis molestias, sunt ratione fuga temporibus architecto magnam aut dolores. Facilis!
+		</p>
+		<button class="button button-pink" type="button">
+			požiadajte o podporu online
+		</button>
+	</div>
+</section>
+
 
 <?php get_footer(); ?>
