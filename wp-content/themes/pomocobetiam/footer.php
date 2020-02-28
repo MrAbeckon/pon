cee3f45b8fb3b3e13caa49dc5c0d9a934310bfec
Martin Albert <albert@killkitsch.com>
2020-02-29 00:44:20 +0100

    base skeleton for footer created

--- wp-content/themes/pomocobetiam/footer.php
+++ wp-content/themes/pomocobetiam/footer.php
@@ -1,7 +1,81 @@
-<footer class="">
-  
-</footer>
+
 </main>
+<footer class="page-footer pt-4">
+
+  <div class="container text-center text-md-left">
+
+    <div class="row">
+      <div class="col-2">
+
+        <img src="<?php echo get_template_directory_uri() ?>/images/pink_logo.png" />
+
+      </div>
+
+      <div class="col-3 offset-1">
+
+        <h4>Lorem</h4>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+
+      <div class="col-3">
+
+        <h4>Lorem</h4>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+        </ul>
+
+      </div>
+
+      <div class="col-3">
+
+        <h4>Lorem</h4>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+        </ul>
+
+      </div>
+
+    </div>
+
+  </div>
+  <div class="container-fluid block-pink p-4">
+    <h2 class="text-center">0944 254 405</h2>
+  </div>
+
+</footer>
 
 <div id="cookieparent">
   <div id="cookieconsent"></div>
