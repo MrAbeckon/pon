451fc506bc3a2fcf4d062151c013370aa8a76243
Martin Albert <albert@killkitsch.com>
2020-03-07 17:56:47 +0100

    functions.php - faq custom post type+font-aws. cdn

--- wp-content/themes/pomocobetiam/functions.php
+++ wp-content/themes/pomocobetiam/functions.php
@@ -50,7 +50,7 @@ function enqueue_styles_scripts() {
   wp_enqueue_style( 'bootstrap-css', get_template_directory_uri().'/stylesheets/bootstrap.min.css');
   wp_enqueue_style ('style', get_template_directory_uri().'/stylesheets/main.css', array(), $css_v);
   // wp_enqueue_style ('style-career', get_template_directory_uri().'/css/style-career.css', array(), $css_v);
-  // wp_enqueue_style ('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style ('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css');
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_styles_scripts' );
 
@@ -131,24 +131,23 @@ remove_action( 'template_redirect', 'wp_shortlink_header', 11 );
 //povolit při zapnutí debbugu
 add_filter('show_admin_bar', '__return_false');
 
-
 // POSTTYPES
 /******************************  PREZENCNE KURZY  ******************************/
-function prezencni_kurzy_init() {
+function faq() {
   $labels = array(
-    'name' => 'Prezenční kurzy',
-    'singular_name' => 'Prezenční kurz',
-    'add_new' => 'Přidat prezenční kurz',
-    'add_new_item' => 'Přidat nový prezenční kurz',
-    'edit_item' => 'Změnit prezenční kurz',
-    'new_item' => 'Přidat prezenční kurz',
-    'all_items' => 'Seznam',
-    'view_item' => 'Zobrazit prezenční kurz',
-    'search_items' => 'Hledat',
-    'not_found' => 'Žádné prezenční kurzy nenalezeny',
-    'not_found_in_trash' => 'Žádné prezenční kurzy nenalezeny',
+    'name' => 'Najčastejšie otázky',
+    'singular_name' => 'Najčastejšia otázka',
+    'add_new' => 'Pridať najčastejšiu otázku',
+    'add_new_item' => 'Pridať novú najčastejšiu otázku',
+    'edit_item' => 'Zmeniť najčastejšiu otázku',
+    'new_item' => 'Pridať najčastejšiu otázku',
+    'all_items' => 'Zoznam najčastejších otázok',
+    'view_item' => 'Zobrazit otázku',
+    'search_items' => 'Hľadať najčastejšie otázky',
+    'not_found' => 'Najčastejšie otázky nenájdené',
+    'not_found_in_trash' => 'Najčastejšie otázky nenájdené',
     'parent_item_colon' => '',
-    'menu_name' => 'Prezenční kurzy'
+    'menu_name' => 'Najčastejšie otázky'
     );
 
   $info = array(
@@ -158,7 +157,7 @@ function prezencni_kurzy_init() {
     'show_ui' => true,
     'show_in_menu' => true,
     'query_var' => true,
-    'rewrite' => array('slug' => 'prezencni-kurzy'),
+    'rewrite' => array('slug' => 'najcastejsie-otazky'),
     'capability_type' => 'post',
     'has_archive' => false,
     'hierarchical' => false,
@@ -167,35 +166,10 @@ function prezencni_kurzy_init() {
     'taxonomies' => array(),
     );
 
-  register_post_type('prezencni-kurzy', $info);
-
-  $labels = array(
-    'name' => 'Kategorie prezenčních kurzů',
-    'singular_name' => 'Kategorie prezenčního kurzu',
-    'add_new' => 'Přidat kategorii prezenčního kurzu',
-    'add_new_item' => 'Přidat novou kategorii prezenčního kurzu',
-    'edit_item' => 'Změnit kategorii prezenčního kurzu',
-    'new_item' => 'Přidat kategorii prezenčního kurzu',
-    'all_items' => 'Seznam kategorii',
-    'view_item' => 'Zobrazit kategorii prezenčního kurzu',
-    'search_items' => 'Hledat kategorii',
-    'not_found' => 'Žádné kategorie prezenčních kurzů nenalezeny',
-    'not_found_in_trash' => 'Žádné kategorie prezenčních kurzů nenalezeny',
-    'parent_item_colon' => '',
-    'menu_name' => 'Kategorie prezenčních kurzů'
-    );
-
-  register_taxonomy('kategorie-prezencnich-kurzu', array('prezencni-kurzy'), array(
-    'hierarchical' => true,
-    'labels' => $labels,
-    'show_ui' => true,
-    'show_admin_column' => true,
-    'query_var' => true,
-    'rewrite' => array( 'slug' => 'kategorie-prezencnich-kurzu'),
-  ));
+  register_post_type('najcastejsie-otazky', $info);
 
 }
-// add_action('init', 'prezencni_kurzy_init');
+add_action('init', 'faq');
 /******************************  end PREZENCNE KURZY  ******************************/
 
 //Změna slova pro stránkování
